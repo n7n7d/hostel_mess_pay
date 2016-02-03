e4d65c45a2a3ccd99b0b2cf23ebdfc913bc7ba84
nischaynamdev <nischay007@gmail.com>
2016-02-03 20:13:20 +0530

    upto static pages

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,40 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "Messpay"
+  end
+
+  test "should get home" do
+    get :home
+    assert_response :success
+    assert_select "title", "Messpay"
+  end
+
+  test "should get help" do
+    get :help
+    assert_response :success
+    assert_select "title", "Help | #{@base_title}"
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
+
+  end
+
+  test "should get developers" do
+    get :developers
+    assert_response :success
+    assert_select "title", "Developers | #{@base_title}"
+
+  end
+end
