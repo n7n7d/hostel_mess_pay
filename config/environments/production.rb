5fdc433654bcd40b957b40a386ce4c6beeaf7d22
nischaynamdev <nischay007@gmail.com>
2016-04-25 01:44:24 +0530

    main changes before deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  RAILS_ENV=production bundle exec rake assets:precompile
   # Code is not reloaded between requests.
   config.cache_classes = true
   config.force_ssl = true
